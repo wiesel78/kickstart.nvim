fcb398b448a6c2e1f4e3a93834afa79b33791ec0
Philip Ullrich <wurst78@gmail.com>
2023-04-01 16:11:27 +0200

    basic configuration

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,15 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+alias dt="datree"
+
+# ALIAS COMMANDS
+alias ll="ls -al"
+alias g="goto"
+alias grep='grep --color'
+
