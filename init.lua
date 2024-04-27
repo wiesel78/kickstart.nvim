7f0cef57c211001dce954726546c0504651abd2d
Philip Ullrich <wurst78@gmail.com>
2024-04-27 20:12:43 +0200

    feat: add search config files

--- init.lua
+++ init.lua
@@ -409,6 +409,10 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      vim.keymap.set('n', '<leader>sc', function()
+        builtin.find_files { cwd = '~/.config' }
+      end, { desc = '[S]earch [C]onfig files' })
     end,
   },
 
