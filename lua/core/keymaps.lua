a778f5fb5d95099a5deb9e79ec1e1c9edb1d5356
Philip Ullrich <wurst78@gmail.com>
2024-05-05 00:45:41 +0200

    feat: add clipboard C-c and C-v stuff

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -93,12 +93,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
@@ -107,3 +101,9 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- clipboard copy with CTRL-c
+vim.keymap.set('n', '<C-c>', '"+', { desc = 'set register + prefix so you can copy with motion and y' })
+vim.keymap.set('n', '<C-v>', '"+p', { desc = 'insert the current system clipboard content' })
+vim.keymap.set('v', '<C-c>', '"+y', { desc = 'Copy current selection into the system clipboard' })
+vim.keymap.set('v', '<C-v>', '"+p', { desc = 'replace current selection with clipboard content' })
