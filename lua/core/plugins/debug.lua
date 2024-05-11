a1afad582cde3f5cacf0f3f6d0d8aa7d47f49b37
Philip Ullrich <wurst78@gmail.com>
2024-05-11 17:47:02 +0200

    chore: small refactoring of nvim config

--- lua/core/plugins/debug.lua
+++ lua/core/plugins/debug.lua
@@ -7,9 +7,7 @@
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
@@ -29,6 +27,8 @@ return {
     local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
+      automatic_installation = true,
+
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
       automatic_setup = true,
